3e2ba7fbeb862e792594215e9e327b9a3a18e50a
Parth20021 <109079250+Parth20021@users.noreply.github.com>
2022-08-09 11:45:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,30 +4,34 @@ let MenuBar = document.getElementById('menu-Bar');
 menuIconMobileView.addEventListener("click", () => {
 MenuBar.classList.toggle('sideBar');
  // Get the current page scroll position
- scrollTop = 
- window.pageYOffset || document.documentElement.scrollTop;
-scrollLeft = 
- window.pageXOffset || document.documentElement.scrollLeft,
+//  scrollTop = 
+//  window.pageYOffset || document.documentElement.scrollTop;
+// scrollLeft = 
+//  window.pageXOffset || document.documentElement.scrollLeft,
 
    // if any scroll is attempted,
    // set this to the previous value
-   window.onscroll = function() {
-       window.scrollTo(scrollLeft, scrollTop);
-    };
-    window.onscroll = function() {};
    if(MenuBar.classList.contains('sideBar')){
-    // Get the current page scroll position
- scrollTop = 
- window.pageYOffset || document.documentElement.scrollTop;
-scrollLeft = 
- window.pageXOffset || document.documentElement.scrollLeft,
-
-   // if any scroll is attempted,
-   // set this to the previous value
-   window.onscroll = function() {
-       window.scrollTo(scrollLeft, scrollTop);
-    };
-   }
+       // Get the current page scroll position
+       scrollTop = 
+       window.pageYOffset || document.documentElement.scrollTop;
+       scrollLeft = 
+       window.pageXOffset || document.documentElement.scrollLeft,
+       
+       // if any scroll is attempted,
+       // set this to the previous value
+       window.onscroll = function() {
+           window.scrollTo(scrollLeft, scrollTop);
+        };
+    }
+    else{
+        /* window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+            window.onscroll = function() {};
+         }; */
+         window.onscroll = function(){};
+        
+     }
 });
 
 
@@ -37,20 +41,24 @@ let bookCover=document.getElementById('book-cover');
 let slideBook=bookTop.offsetTop;
 
 // console.log(slideBook);
+window.onscroll = function() { function2() };
 
-window.onscroll = () => {
+function function2(){
+    
+    if(!MenuBar.classList.contains('sideBar')){
+    // console.log(window.pageYOffset);
 // console.log(window.scrollY >=160);
 // console.log(window.scrollY >=slideBook);
-if(window.scrollY >=50){
+if(window.scrollY >=200){
     bookCover.classList.add("MoveVertical");
 }
 else{
-    
-    
+
     bookCover.classList.remove("MoveVertical");
 }
 
 // if(w)
+}
 }
 // if(window.scrollTop)
 
